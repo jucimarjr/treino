9c97ca2deecc1e57e5250982451a1945c4c114b0
0vrr <kaioccs@gmail.com>
2018-01-27 02:06:29 -0200

    Enviando o Exerc01

---
+++ lista2/01.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if (num1 > num2):
+    print (num1, 'eh maior que', num2)
+elif (num1 < num2):
+    print (num2, 'eh maior que', num1)
+else:
+    print ('Os numeros sao iguais')
