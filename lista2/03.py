7595412065115bf708486fe664a5f92378e1f8c3
0vrr <kaioccs@gmail.com>
2018-01-28 12:51:19 -0200

    Envinando Exerc03

---
+++ lista2/03.py
@@ -0,0 +1,9 @@
+sex = input("Informe F para feminino e M para masculino: ")
+if (sex.upper() == "M"):
+    print ("Masculino")
+
+elif(sex.upper() == "F"):
+    print ("Feminino")
+
+else:
+    print("Sexo Inválido")
