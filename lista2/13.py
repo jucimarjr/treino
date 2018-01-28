665602344fa2fb8f9013eb05d9dbc38229864961
0vrr <kaioccs@gmail.com>
2018-01-28 18:47:11 -0200

    Enviando Exerc13

---
+++ lista2/13.py
@@ -0,0 +1,18 @@
+numero_dia = int(input("Informe a semana em forma de número: "))
+
+if (numero_dia == 1):
+    print ("Domingo")
+elif (numero_dia == 2):
+    print ("Segunda")
+elif (numero_dia == 3):
+    print ("Terça")
+elif (numero_dia == 4):
+    print ("Quarta")
+elif (numero_dia == 5):
+    print ("Quinta")
+elif (numero_dia == 6):
+    print ("Sexta")
+elif (numero_dia == 7):
+    print ("Sábado")
+else:
+    print ("Informe um dia da semana válido")
