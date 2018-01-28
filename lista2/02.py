447468570a2f1dd19a22224da6dfd454bef8fb31
0vrr <kaioccs@gmail.com>
2018-01-28 12:47:03 -0200

    Envinando Exerc02

---
+++ lista2/02.py
@@ -0,0 +1,14 @@
+print ("Exerc 2")
+
+num = int(input("Informe um número: "))
+
+if (num > 0):
+    print ("O número",num,"é positivo")
+
+
+elif (num < 0):
+    print ("O número",num,"é negativo")
+
+
+else :
+    print ("O número é igual a zero")
