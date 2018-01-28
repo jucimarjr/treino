73d0fca465b5cfd651f25c6b6c74908966827384
0vrr <kaioccs@gmail.com>
2018-01-28 13:17:51 -0200

    Enviando exerc07

---
+++ lista2/07.py
@@ -0,0 +1,22 @@
+print ("Exerc07")
+
+num1 = int(input("Informe um número: "))
+num2 = int(input("Informe o segundo número: "))
+num3 = int(input("Informe o terceiro número: "))
+
+if (num1 == num2) and (num1 == num3):
+    print ("Os números são iguais")
+else:
+    if(num1 > num2) and (num1 > num3):
+        print ("O maior número é",num1,",o primeiro")
+    elif(num2 > num3):
+        print ("O maior número é",num2,",o segundo")
+    else:
+        print("O maior número é",num3,",o terceiro")
+    
+if (num1 < num2) and (num1 < num3):
+    print("O menor número é",num1,",o primeiro")
+elif (num2 < num3):
+    print("O menor número é",num2,",o segundo")
+else:
+    print("O menor número é",num3,",o terceiro")
