9fde6badb851ffcfe0c37ac19b7cc1dbf8888755
0vrr <kaioccs@gmail.com>
2018-01-29 16:23:00 -0200

    Exerc07

---
+++ lista3/07.py
@@ -0,0 +1,11 @@
+for i in range (0,5):
+    num = int(input("Informe um número: "))
+    if ("maior" in vars()):
+              if (num > maior):
+                  maior = num
+    else:
+              maior = num
+
+              
+print("O maior numero que você digitou é",maior)
+              
