fc4714f8be3e70663a0980a710443e2d4c11dd8f
0vrr <kaioccs@gmail.com>
2018-01-29 16:31:26 -0200

    Exerc08

---
+++ lista3/08.py
@@ -0,0 +1,7 @@
+soma = 0
+for i in range(0, 5):
+    num = int(input("Informe um número: "))
+    soma += num
+
+print ('A soma dos números digitados e', soma)
+print ('A média dos números digitados é', (soma / 5.0))
