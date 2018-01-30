1ace2343c2b00a6186edc29aad84fb6946f4b174
0vrr <kaioccs@gmail.com>
2018-01-30 17:37:46 -0200

    Exerc22

---
+++ lista3/22.py
@@ -0,0 +1,14 @@
+n = int(input('Digite um número inteiro: '))
+c = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+       c += 1
+if c == 2:
+    print(' O número {} é primo'.format(n),'pois ele só é divisivel por 1 e por ele mesmo')
+else:
+    print('O número {} não é primo'.format(n),'pois ele e divisivel por',
+          for i in range(1, n + 1):
+          if (n % i == 0):
+            print i)
+
+          
